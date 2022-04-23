f6c439d224b54f17039bf2c5828877c0a2ece8a8
CameronAGluth <90208438+CameronAGluth@users.noreply.github.com>
2022-04-23 14:14:31 -0500

    google maps infrastructure

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 function init(){
-  var entryinput = document.getElementById("entity");
-  if (input_text.value == "") {
-      alert("Please enter an entity.");
-      return false;
-  }
-  alert("Hello " + entryinput + "!");
-  return true
+  var el entryinput = document.getElementById("canvas");
+  var myLocation = new google.maps.LatLng(41.835117, -87.627130);
+  var mapOptions = {
+    center: myLocation,
+    zoom: 18,
+    mapTypeId: google.maps.ControlPosition.BOTTOM_CENTER
+    }
+  };
+
+  var myMap = new google.maps.Map(el, mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myLocation,
+    map: myMap,
+    animation: google.maps.Animation.BOUNCE,
+    icon: 'iit-icon.png'
+  });
+  // if (input_text.value == "") {
+  //     alert("Please enter an entity.");
+  //     return false;
+  // }
+  // alert("Hello " + entryinput + "!");
+  // return true
 }
 //add your JavaScript between these two lines of code
 
