5c05fcd870205d1a4a2e66b6531584bae90b9c2a
CameronAGluth <90208438+CameronAGluth@users.noreply.github.com>
2022-04-25 17:42:08 -0500

    WIP (scripting Google Maps)

--- script.js
+++ script.js
@@ -1,55 +1,10 @@
-// function init() {
-//     'use strict';
-    /*jslint browser:true */
-    // let map;
+let map;
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById("map"), {
+  map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
 }
 
 window.initMap = initMap;
-//     var entryinput = document.getElementById("canvas"),
-//         myLocation = new google.maps.LatLng(41.835117, -87.627130),
-//         mapOptions = {
-//             center: myLocation,
-//             zoom: 18,
-//             mapTypeId: google.maps.ControlPosition.BOTTOM_CENTER
-//         },
-//
-//
-//         myMap = new google.maps.Map(el, mapOptions),
-//
-//         marker = new google.maps.Marker({
-//             position: myLocation,
-//             map: myMap,
-//             animation: google.maps.Animation.BOUNCE,
-//             icon: 'iit-icon.png'
-//         });
-//
-//
-//     alert("IT WORKS!");
-// }
-//
-// google.maps.event.addDomListener(window, 'load', init);
-
-
-  // if (input_text.value == "") {
-  //     alert("Please enter an entity.");
-  //     return false;
-  // }
-  // alert("Hello " + entryinput + "!");
-  // return true
-
-//add your JavaScript between these two lines of code
-
-
-
-
-
-
-
-
-// window.addEventListener('load', init);
